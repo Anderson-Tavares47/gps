0fb88b4ab94166f5593f42cb3d8fea7a458d36fc
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-07-31 20:02:09 -0300

    Update getRoute.js

--- src/utils/getRoute.js
+++ src/utils/getRoute.js
@@ -1,8 +1,7 @@
-// utils/getRoute.js
 import axios from 'axios';
 
 export const getRoutes = async (start, end) => {
-  const response = await axios.get(`http://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&alternatives=true&geometries=geojson`);
+  const response = await axios.get(`https://router.project-osrm.org/route/v1/driving/${start[1]},${start[0]};${end[1]},${end[0]}?overview=full&alternatives=true&geometries=geojson`);
   const routes = response.data.routes.map(route => ({
     coordinates: route.geometry.coordinates.map(coord => [coord[1], coord[0]]),
     duration: route.duration // duração em segundos
