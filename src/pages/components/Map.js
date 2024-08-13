d3433221614c4c4599f25bb99482159835d5fe5b
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-08-13 00:36:19 -0300

    Update Map.js

--- src/pages/components/Map.js
+++ src/pages/components/Map.js
@@ -2,8 +2,10 @@ import dynamic from 'next/dynamic';
 
 const DynamicMap = dynamic(() => import('./DynamicMap'), { ssr: false });
 
-const MapWrapper = ({ routeCoordinates = [] }) => {
-  return <DynamicMap routeCoordinates={routeCoordinates} />;
+const MapWrapper = ({ allRoutes = [], radius }) => {
+  console.log("MapWrapper - allRoutes:", allRoutes); // Log para verificar as rotas
+  console.log("MapWrapper - radius:", radius); // Log para verificar o raio
+  return <DynamicMap allRoutes={allRoutes} radius={radius} />;
 };
 
 export default MapWrapper;
