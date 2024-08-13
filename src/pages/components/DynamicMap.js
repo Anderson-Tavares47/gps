acbaeeb1c97dc4e1e48fa57895e593dffbfe185c
Anderson-Tavares47 <67126943+Anderson-Tavares47@users.noreply.github.com>
2024-08-13 00:36:07 -0300

    Update DynamicMap.js

--- src/pages/components/DynamicMap.js
+++ src/pages/components/DynamicMap.js
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as turf from '@turf/turf';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Polygon, Polyline, Marker, Tooltip, useMapEvents } from 'react-leaflet';
 
-const DynamicMap = ({ routeCoordinates = [] }) => {
+const DynamicMap = ({ allRoutes = [], radius }) => {
+  console.log("Received routes with radius:", allRoutes); // Verifica se as rotas e raios estão sendo recebidos
+  console.log("Default radius:", radius); // Verifica o raio padrão recebido
+
   const [leaflet, setLeaflet] = useState(null);
   const [mapComponents, setMapComponents] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
-  const [fencePolygon, setFencePolygon] = useState(null);
+  const [fencePolygons, setFencePolygons] = useState([]);
 
   useEffect(() => {
     const loadLeaflet = async () => {
@@ -16,21 +21,15 @@ const DynamicMap = ({ routeCoordinates = [] }) => {
             import('leaflet'),
             import('react-leaflet')
           ]);
-
-          const markerIconPng = await import('leaflet/dist/images/marker-icon.png');
-          const markerShadowPng = await import('leaflet/dist/images/marker-shadow.png');
-
           const defaultIcon = L.icon({
-            iconUrl: markerIconPng.default,
-            shadowUrl: markerShadowPng.default,
+            iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
             iconSize: [25, 41],
             iconAnchor: [12, 41],
             popupAnchor: [1, -34],
             shadowSize: [41, 41],
           });
-
           L.Marker.prototype.options.icon = defaultIcon;
-
           setLeaflet(L);
           setMapComponents(reactLeaflet);
         } catch (error) {
@@ -38,35 +37,31 @@ const DynamicMap = ({ routeCoordinates = [] }) => {
         }
       }
     };
-
     loadLeaflet();
   }, []);
 
-  const createFencePolygon = (coords, bufferDistance) => {
-    if (coords.length < 2) {
-      return null; // Retorna null se não houver pontos suficientes para criar uma linha
+  useEffect(() => {
+    const processRoutes = (routes) => {
+      const routeArray = Array.isArray(routes) ? routes : [routes];
+      const polygons = routeArray.map(route => {
+        const routeRadius = route.radius ? parseFloat(route.radius) : radius;
+        return route.coordinates ? createFencePolygon(route.coordinates, routeRadius) : null;
+      });
+      setFencePolygons(polygons);
+    };
+
+    if (allRoutes) {
+      processRoutes(allRoutes);
     }
-    // Converte as coordenadas para o formato [longitude, latitude]
+  }, [allRoutes, radius]);
+
+  const createFencePolygon = (coords, bufferDistance) => {
+    if (!coords || coords.length < 2) return null;
     const lineCoords = coords.map(coord => [coord[1], coord[0]]);
     const line = turf.lineString(lineCoords);
     const buffered = turf.buffer(line, bufferDistance, { units: 'kilometers' });
     return buffered.geometry.coordinates[0];
   };
-
-  useEffect(() => {
-    const newFencePolygon = createFencePolygon(routeCoordinates, 3);
-    setFencePolygon(newFencePolygon);
-
-    if (newFencePolygon && userLocation) {
-      const isInsideFence = turf.booleanPointInPolygon(turf.point(userLocation), turf.polygon([newFencePolygon]));
-
-      // if (!isInsideFence) {
-      //   console.log('Você saiu da cerca de 3 km após definir a rota!');
-      // } else {
-      //   console.log('Você está dentro da cerca de 3 km após definir a rota.');
-      // }
-    }
-  }, [routeCoordinates, userLocation]);
 
   if (!leaflet || !mapComponents) {
     return <div>Loading map...</div>;
@@ -74,21 +69,10 @@ const DynamicMap = ({ routeCoordinates = [] }) => {
 
   const { MapContainer, TileLayer, Polygon, Polyline, Marker, Tooltip, useMapEvents } = mapComponents;
 
-  const LocationMarker = ({ fencePolygon }) => {
+  const LocationMarker = () => {
     const map = useMapEvents({
       locationfound(e) {
-        const userLocation = [e.latlng.lng, e.latlng.lat];
-        setUserLocation([e.latlng.lat, e.latlng.lng]); // Atualiza a localização do usuário
-
-        if (fencePolygon && fencePolygon.length >= 4) {
-          const isInsideFence = turf.booleanPointInPolygon(turf.point(userLocation), turf.polygon([fencePolygon]));
-
-          if (!isInsideFence) {
-            console.log('Você saiu da cerca de 3 km!');
-          } else {
-            console.log('Você está dentro da cerca de 3 km.');
-          }
-        }
+        setUserLocation([e.latlng.lat, e.latlng.lng]);
       }
     });
 
@@ -99,34 +83,34 @@ const DynamicMap = ({ routeCoordinates = [] }) => {
     return null;
   };
 
-  // Removendo o useEffect para invalidateSize pois não é necessário
-  // e pode causar erros na renderização de hooks
+  const defaultCenter = [-30.0346, -51.2177];
+  const routeArray = Array.isArray(allRoutes) ? allRoutes : [allRoutes];
+  const firstRoute = routeArray[0];
+  const mapCenter = userLocation || (firstRoute && firstRoute.coordinates && firstRoute.coordinates.length > 0 ? [firstRoute.coordinates[0][0], firstRoute.coordinates[0][1]] : defaultCenter);
 
   return (
-    <MapContainer id="map" center={userLocation || routeCoordinates[0] || [-30.0346, -51.2177]} zoom={6} style={{ height: '100vh', width: '100%' }}>
+    <MapContainer center={mapCenter} zoom={6} style={{ height: '100vh', width: '100%' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      {routeCoordinates.length > 0 && (
-        <>
-          <Polyline positions={routeCoordinates} color="black" />
-          {fencePolygon && fencePolygon.length >= 4 && (
+      {routeArray.map((route, index) => (
+        <React.Fragment key={`route-group-${index}`}>
+          {route.coordinates && (
+            <Polyline positions={route.coordinates.map(coord => [coord[0], coord[1]])} color="black"/>
+          )}
+          {fencePolygons[index] && (
             <Polygon
-              positions={fencePolygon.map(coord => [coord[1], coord[0]])}
-              pathOptions={{
-                color: 'red',
-                fillColor: 'none',
-                fillOpacity: 0,
-              }}
+              positions={fencePolygons[index].map(coord => [coord[1], coord[0]])}
+              pathOptions={{ color: 'red', fillColor: 'none', fillOpacity: 0.5 }}
             />
           )}
-        </>
-      )}
-      <LocationMarker fencePolygon={fencePolygon} />
+        </React.Fragment>
+      ))}
+      <LocationMarker />
       {userLocation && (
-        <Marker 
-          position={userLocation} 
+        <Marker
+          position={userLocation}
           icon={leaflet.icon({
             iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
             shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
@@ -136,9 +120,7 @@ const DynamicMap = ({ routeCoordinates = [] }) => {
             shadowSize: [41, 41]
           })}
         >
-          <Tooltip>
-            <span>Você está aqui</span>
-          </Tooltip>
+          <Tooltip><span>Você está aqui</span></Tooltip>
         </Marker>
       )}
     </MapContainer>
